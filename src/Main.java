bf53c87b1c58fbd7301f181e9a7f81bd2f9e9567
rickyeeez <wardhanaricky30@gmail.com>
2023-03-24 08:31:46 +0700

    first commit

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,35 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         ArrayList<String> hewan = new ArrayList<String>();
-        ArrayList<String> deleteHewan = new ArrayList<String>();
+        ArrayList<String> deletedHewan = new ArrayList<String>();
         Scanner input = new Scanner(System.in);
-        hewan.add("Kuda");
-        hewan.add("Kelinci");
-        hewan.add("Kambing");
-        hewan.add("Unta");
-        hewan.add("Domba");
-
-        //Menambahkan Delete Hewan
-        deleteHewan.add("Kelinci");
-        deleteHewan.add("Kambing");
-        deleteHewan.add("Unta");
+        String myInput;
+        System.out.printf("Masukkan Hewan : ");
+        myInput = input.nextLine();
+        myInput = myInput.replaceAll("\\[", "");
+        myInput = myInput.replaceAll("\\]", "");
+        String data[];
+        data = myInput.split(",");
+        for (String i : data) {
+            hewan.add(i);
+        }
+        System.out.print("Hewan yang dihapus : ");
+        String delHewan;
+        delHewan = input.nextLine();
+        delHewan = delHewan.replaceAll("\\[", "");
+        delHewan = delHewan.replaceAll("\\]", "");
+        data = null;
+        data = delHewan.split(",");
+        for (String i : data) {
+            deletedHewan.add(i);
+        }
 
         System.out.println("Hewan : " + hewan);
-        System.out.println("Hewan Yang Dihapus : " + deleteHewan);
+        System.out.println("Hewan Yang Dihapus : " + deletedHewan);
 
 
-
-        Iterator<String> iterator = hewan.iterator();
-        while (iterator.hasNext()) {
-            String h = iterator.next();
-            for (String i : deleteHewan) {
-                if (i.equals(h)) {
-                    iterator.remove();
-                    break;
-                }
-            }
+        for (int i = 0; i < deletedHewan.size(); i++) {
+            hewan.remove(deletedHewan.get(i));
         }
         System.out.println("Output hewan : " + hewan);
     }
